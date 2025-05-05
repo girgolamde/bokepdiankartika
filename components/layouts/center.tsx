4d35a309c50cbcdcf16803a968ed0c4dbb1929c1
girgolamde <girgolamde@gmail.com>
2025-05-05 09:11:13 +0700

    Update center.tsx

--- components/layouts/center.tsx
+++ components/layouts/center.tsx
@@ -3,6 +3,11 @@ import React from "react";
 const Center = ({ children }: { children: React.ReactNode }) => {
     return (
          <div className="flex flex-col h-[50vh] justify-center items-center">
+<script
+      dangerouslySetInnerHTML={{
+         __html: `(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1078976})`,
+      }}
+   /><Script data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"/><ins id="1078976" data-width="300" data-height="112"></ins>
             {children}
         </div>
     );
