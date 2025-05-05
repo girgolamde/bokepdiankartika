db1e3e94ac7935f5a4064921653cbc792b7b27cc
girgolamde <girgolamde@gmail.com>
2025-05-05 09:38:39 +0700

    Update center.tsx

--- components/layouts/center.tsx
+++ components/layouts/center.tsx
@@ -4,11 +4,6 @@ import Script from "next/script";
 const Center = ({ children }: { children: React.ReactNode }) => {
     return (
          <div className="flex flex-col h-[50vh] justify-center items-center">
-<script
-      dangerouslySetInnerHTML={{
-         __html: `(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1078976})`,
-      }}
-   /><Script data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"/>
             {children}
         </div>
     );
